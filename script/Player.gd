5370813a3be3d9b90ca36e35e70c028845a4083e
soundtrack42 <ribicgiorge@gmail.com>
2023-05-13 11:48:13 +0200

    Player's Script

--- script/Player.gd
+++ script/Player.gd
@@ -4,49 +4,57 @@ extends CharacterBody2D
 const SPEED = 30.0;
 var x;
 var y;
+@onready var animation = $AnimationPlayer
+@export var mappa : TileMap
 
+func _ready():
+	pass
 
 func _physics_process(delta):
 	
 	if Input.is_key_pressed(KEY_W):
 		y-=SPEED;
-		$AnimationPlayer.play("move_up");
+		animation.play("move_up");
 	elif Input.is_key_pressed(KEY_S):
 		y+=SPEED;
-		$AnimationPlayer.play("move_down");
+		animation.play("move_down");
 	elif Input.is_key_pressed(KEY_A):
 		x-=SPEED;
-		$AnimationPlayer.play("move_left");
+		animation.play("move_left");
 	elif Input.is_key_pressed(KEY_D):
 		x+=SPEED;
-		$AnimationPlayer.play("move_right");
+		animation.play("move_right");
 	elif Input.is_key_pressed(KEY_S) && Input.is_key_pressed(KEY_L):
 		x-=SPEED;
 		y+=SPEED;
-		$AnimationPlayer.play("move_up_right");
+		animation.play("move_up_right");
 	elif Input.is_key_pressed(KEY_S) && Input.is_key_pressed(KEY_D):
 		x+=SPEED;
 		y+=SPEED;
-		$AnimationPlayer.play("move_down_right");
+		animation.play("move_down_right");
 	elif Input.is_key_pressed(KEY_W) && Input.is_key_pressed(KEY_A):
 		x-=SPEED;
 		y-=SPEED;
-		$AnimationPlayer.play("move_up_left");
+		animation.play("move_up_left");
 	elif Input.is_key_pressed(KEY_W) && Input.is_key_pressed(KEY_D):
 		x+=SPEED;
 		y-=SPEED;
-		$AnimationPlayer.play("move_down_left");
+		animation.play("move_down_left");
 	elif Input.is_key_pressed(KEY_Z):
+		if mappa.world_to_map(mappa.to_local(Vector2(x,y))) == "":
+			animation.play("zappa");
 		
-		$AnimationPlayer.play("zappa");
 	elif Input.is_key_pressed(KEY_X):
+		if mappa.world_to_map(mappa.to_local(Vector2(x,y))) == "":
+			animation.play("annaffia");
 		
-		$AnimationPlayer.play("annaffia");
+		
 	elif Input.is_key_pressed(KEY_C):
+		if mappa.world_to_map(mappa.to_local(Vector2(x,y))) == "":
+			animation.play("raccogli");
 		
-		$AnimationPlayer.play("raccogli");
 	else:
-		$AnimationPlayer.play("idle");
+		animation.play("idle");
 	
 	
 	
